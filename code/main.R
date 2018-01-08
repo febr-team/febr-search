a09edb172f577f324572288fe95138ed2534a7db
samuel-rosa <alessandrosamuelrosa@gmail.com>
2018-01-08 09:34:14 -0200

    source code

---
+++ code/main.R
@@ -0,0 +1,18 @@
+# Render site
+
+# Processar arquivo Rmd e gerar saída em HTML
+rmarkdown::render('code/search.Rmd', encoding = 'UTF-8')
+
+# Editar arquivo HTML
+con <- 'code/search.html'
+file <- readLines(con = con)
+for (i in seq(length(file))) {
+  
+  # Ajustar largura da tabela à largura da tela.
+  file[i] <- gsub(pattern = 'max-width: 940px;', replacement = 'max-width: 100%;', x = file[i], fixed = TRUE)
+  
+  # Configuração do idioma do filtro das colunas.
+  file[i] <- 
+    gsub(pattern = 'placeholder=\\"All\\"', replacement = 'placeholder=\\"Todos\\"', x = file[i], fixed = TRUE)
+}
+writeLines(text = file, con = con)
